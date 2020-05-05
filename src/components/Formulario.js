013608b47bcfe4f9becec5db2b4a4d20949d629c
Victor Moya <vjmoyaparedes@gmail.com>
2020-05-05 18:09:16 -0300

    first commit

---
+++ src/components/Formulario.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import useMoneda from '../hooks/useMoneda'; 
+
+
+const Boton = styled.input`
+    margin-top:20px;
+    font-weight:bold;
+    font-size:20px;
+    padding:10px;
+    background-color:#66a2fe;
+    border:none;
+    width:100%;
+    border-radius:10px;
+    color:#FFF;
+    transition: background-color .3s ease;
+
+    &:hover{
+        background-color:#326AC0;
+        cursor:pointer;
+    }
+`;
+
+
+
+const Formulario = () => {
+
+    //Utilizar useMoneda
+    const [moneda, SelectMonedas, actualizarState]= useMoneda();
+
+
+    return ( 
+        <div>
+            <form>
+                <SelectMonedas
+                    
+                />
+                <Boton
+                    type="submit"
+                    value="Calcular"
+                />
+            </form>
+        </div>
+
+     );
+}
+ 
+export default Formulario;
